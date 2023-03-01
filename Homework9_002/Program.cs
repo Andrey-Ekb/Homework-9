64ead9fcd6d34cdc49528aefe2dbf1e0d78f600a
Andrey <Andre.68.1@mail.ru>
2023-03-01 18:31:29 +0500

    sum of elements

---
+++ Homework9_002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumElement(int a, int b, int sum)
+{
+    if (a <= b)
+    { 
+        sum = sum + a;
+        a++;
+        Console.Write(sum+"  ");
+        SumElement(a, b, sum);
+    } 
+return sum;
+}
+
+int a=10;
+int b=25;
+int sum=0;
+Console.WriteLine();
+SumElement( a, b, sum);
+Console.WriteLine();
+Console.WriteLine();
+
+
