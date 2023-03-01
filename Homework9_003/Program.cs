0b738b4785ec479ead269f4ed74d4a3a5f42796c
Andrey <Andre.68.1@mail.ru>
2023-03-01 18:53:05 +0500

    sum of a number

---
+++ Homework9_003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму цифр числа
+
+int SumNumber (int number)
+{
+    if(number<1) return 0;
+    return SumNumber(number/10)+number%10;
+}
+
+Console.Clear();
+Console.WriteLine();
+Console.Write("Введите N: ");
+int number=int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.WriteLine(SumNumber(number));
+Console.WriteLine();
