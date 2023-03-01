7a125f90167656149d9c27abfef038b2053a1bbd
Andrey <Andre.68.1@mail.ru>
2023-03-01 14:47:02 +0500

    output of natural numbers

---
+++ Homework9_001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void PrintTill (int n)
+{
+    if(n<=25)
+    { 
+        PrintTill (n+1);
+        Console.Write($"{n}\t");
+    }
+}
+int n=10;
+Console.Clear();
+Console.WriteLine();
+PrintTill (n);
+Console.WriteLine();
+Console.WriteLine();
