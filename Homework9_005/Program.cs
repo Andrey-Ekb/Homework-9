45215babaa30a59b30ee74926e923581f2d157ed
Andrey <Andre.68.1@mail.ru>
2023-03-02 14:16:47 +0500

    Raising a number to a power

---
+++ Homework9_005/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу возведения числа А в целую стень B
+int s (int b, int a, int sum)
+{
+    if (b!=0) return s(b - 1,a,sum=sum*a);
+    Console.WriteLine();
+    Console.WriteLine(sum);
+    return 0;
+}
+
+Console.Clear();
+int sum=1;
+Console.Write("Введите число : ");
+int a=int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень : ");
+int b=int.Parse(Console.ReadLine()!);
+
+s(b,a,sum);
+Console.WriteLine();
